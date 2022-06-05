31eefe17538e9d038ebafec1b3a6f0e268bd9e64
Sudipta Jana <95089569+sudip31@users.noreply.github.com>
2022-06-05 17:09:47 +0530

    Update NthPrime.js

--- June/week1/test1/NthPrime.js
+++ June/week1/test1/NthPrime.js
@@ -1,15 +1,15 @@
-// let n = parseInt(prompt("enter the term"))
-// let num= 1; count=0;
-// while(count<n){
-//     num = num+1;
-//     for(i=2;i<=num;i++){
-//         if(num%i == 0){
-//             break;
-//         }
-//     }
-//     if(i==num){
-//         count = count+1;
-//     }
-// }
-// console.log(num)
+let n = parseInt(prompt("enter the term"))
+let num= 1; count=0;
+while(count<n){
+    num = num+1;
+    for(i=2;i<=num;i++){
+        if(num%i == 0){
+            break;
+        }
+    }
+    if(i==num){
+        count = count+1;
+    }
+}
+console.log(num)
 
