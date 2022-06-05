7b89947acf6433c7571a85dbcac9a0f2bb1900b7
Sudipta Jana <95089569+sudip31@users.noreply.github.com>
2022-06-05 17:09:20 +0530

    Update oddsum.js

--- June/week1/test1/oddsum.js
+++ June/week1/test1/oddsum.js
@@ -1,10 +1,10 @@
-// let n = parseInt(prompt("enter terms"));
-// let sum = 0;
+let n = parseInt(prompt("enter terms"));
+let sum = 0;
 
-// for(i=1;i<=n;i++){
-//     console.log("the odd number is",2*i-1)
-//     sum+= 2*i-1 
+for(i=1;i<=n;i++){
+    console.log("the odd number is",2*i-1)
+    sum+= 2*i-1 
 
-// }
+}
 
-// console.log("sum of the odd numbers is", sum);
+console.log("sum of the odd numbers is", sum);
