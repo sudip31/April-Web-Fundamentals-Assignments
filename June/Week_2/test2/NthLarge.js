452a4057561f0ebbd02b4ea17265520b5bc8f2af
Sudipta Jana <95089569+sudip31@users.noreply.github.com>
2022-06-12 19:43:27 +0530

    Update NthLarge.js

--- June/Week_2/test2/NthLarge.js
+++ June/Week_2/test2/NthLarge.js
@@ -1,18 +1,18 @@
-// let arr=[12,34,13,11,15,54,66,3]
-// let temp;
-// for(let i=0;i<arr.length;i++){
-//  for (let j = i+1; j < arr.length; j++) {
-//     if (arr[i] < arr[j]) {
-//       // arr[0] = arr[i];
+let arr=[12,34,13,11,15,54,66,3]
+let temp;
+for(let i=0;i<arr.length;i++){
+ for (let j = i+1; j < arr.length; j++) {
+    if (arr[i] < arr[j]) {
+      // arr[0] = arr[i];
          
-//       temp    =arr[i]; 
-//       arr[i]  =arr[j]; 
-//       arr[j]  =temp;
-//     }
-//   }
-// }
-//   let s = parseInt(prompt("enter n_th highest number position"))
-//   console.log(arr[s])
+      temp    =arr[i]; 
+      arr[i]  =arr[j]; 
+      arr[j]  =temp;
+    }
+  }
+}
+  let s = parseInt(prompt("enter n_th highest number position"))
+  console.log(arr[s])
 
 
 
