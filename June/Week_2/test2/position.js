6d7097ee7e03273707354d1ed4ac352a114e12a1
Sudipta Jana <95089569+sudip31@users.noreply.github.com>
2022-06-12 19:41:57 +0530

    Update position.js

--- June/Week_2/test2/position.js
+++ June/Week_2/test2/position.js
@@ -1,6 +1,6 @@
-// var arr = ['a', 'b', 'c','d']
-// for(i=0;i<arr.length;i++){
-//   if(arr[i]=='b'){
-//     console.log(i)
-//   }
-// }
+var arr = ['a', 'b', 'c','d']
+for(i=0;i<arr.length;i++){
+  if(arr[i]=='b'){
+    console.log(i)
+  }
+}
