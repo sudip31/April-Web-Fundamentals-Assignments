1d4a954dc912c96470e8eb1b797c29f4d02ca38e
Sudipta Jana <95089569+sudip31@users.noreply.github.com>
2022-06-12 19:33:40 +0530

    Add files via upload

---
+++ June/Week_2/test2/arrr.js
@@ -0,0 +1,12 @@
+var arr1 = [1,2,3,]; 
+console.log(arr1.length);
+var arr2 = new Array(3);
+console.log(arr2.length);
+var arr3 = [1,2,3,4,5]
+console.log(arr3.length);
+var array = [ [1,2,3], [4,5,6]  ];
+console.log(array.length);
+let lengthOfZeroIndex = array[0].length
+console.log(lengthOfZeroIndex);
+
+
---
+++ June/Week_2/test2/position.js
@@ -0,0 +1,6 @@
+// var arr = ['a', 'b', 'c','d']
+// for(i=0;i<arr.length;i++){
+//   if(arr[i]=='b'){
+//     console.log(i)
+//   }
+// }
